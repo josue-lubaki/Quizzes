e662169d3409092cce07fa6394d00bc221a59a0c
Josue Lubaki <josuelubaki@gmail.com>
2022-12-05 05:21:31 -0500

    added RecyclerView Stats xml

--- app/src/main/java/ca/josue/mainactivity/MainActivity.java
+++ app/src/main/java/ca/josue/mainactivity/MainActivity.java
@@ -10,7 +10,6 @@ import android.view.WindowManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
-import androidx.multidex.MultiDex;
 
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
@@ -19,6 +18,7 @@ import java.util.Objects;
 import ca.josue.mainactivity.databinding.ActivityMainBinding;
 import ca.josue.mainactivity.ui.fragments.Home;
 import ca.josue.mainactivity.ui.fragments.Pregame;
+import ca.josue.mainactivity.ui.fragments.Stats;
 import ca.josue.mainactivity.utils.Menu;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,53 +30,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        initScreenConfig();
+        configureNavigationBar();
+        checkBundleData();
+    }
+
+    private void initScreenConfig() {
         // Hide the status bar. and make the app fullscreen
         Objects.requireNonNull(getSupportActionBar()).hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        navBar = binding.navBar;
-        configureNavigationBar();
-
-        Bundle bundle = getIntent().getExtras();
-        if (bundle == null) return;
-
-        int menu = bundle.getInt(GAME_NOTIFICATION);
-        if (menu != 0 && menu == Menu.PREGAME.getId()) {
-            navBar.show(Menu.PREGAME.getId(), false);
-            showFragment(Pregame.class);
-        }
-    }
-
-    public void changeNavBar(Menu menu, boolean animation) {
-        navBar.show(menu.getId(), animation);
     }
 
     private void configureNavigationBar() {
+        navBar = binding.navBar;
         navBar.add(new MeowBottomNavigation.Model(Menu.HOME.getId(),R.drawable.ic_home));
         navBar.add(new MeowBottomNavigation.Model(Menu.PREGAME.getId(),R.drawable.ic_game));
-        navBar.add(new MeowBottomNavigation.Model(Menu.PROFILE.getId(), R.drawable.ic_profile));
+        navBar.add(new MeowBottomNavigation.Model(Menu.PROFILE.getId(), R.drawable.ic_score));
         navBar.setOnShowListener(item -> {
             Class<? extends Fragment> fragment = null;
             switch(item.getId()){
                 case 1:
                     fragment = Home.class;
-                    Log.i(TAG,"show Home.class");
                     break;
                 case 2:
                     fragment = Pregame.class;
                     break;
                 case 3:
-                    fragment = Home.class;
-                    Log.i(TAG,"show Profile.class");
+                    fragment = Stats.class;
                     break;
             }
             assert fragment != null;
             showFragment(fragment);
         });
 
-        navBar.show(1,true);
+        changeNavBar(Menu.HOME,true);
         navBar.setOnClickMenuListener(item -> {});
         navBar.setOnReselectListener(item -> {
             if(item.getId() == Menu.HOME.getId()){
@@ -85,7 +74,17 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    /*****************  Affichage des Fragments  *****************/
+    private void checkBundleData() {
+        Bundle bundle = getIntent().getExtras();
+        if (bundle == null) return;
+
+        int menu = bundle.getInt(GAME_NOTIFICATION);
+        if (menu != 0 && menu == Menu.PREGAME.getId()) {
+            changeNavBar(Menu.PREGAME, false);
+            showFragment(Pregame.class);
+        }
+    }
+
     public void showFragment(Class<? extends Fragment> fragment) {
         try {
             Fragment currentFragment = getSupportFragmentManager().findFragmentByTag(fragment.getName());
@@ -99,8 +98,12 @@ public class MainActivity extends AppCompatActivity {
 
         } catch(InstantiationException | IllegalAccessException e){
             e.printStackTrace();
-            Log.d(TAG,"erreur au moment d'instancier fragment");
+            Log.d(TAG,"Error while creating fragment");
         }
+    }
+
+    public void changeNavBar(Menu menu, boolean animation) {
+        navBar.show(menu.getId(), animation);
     }
 
     @Override
